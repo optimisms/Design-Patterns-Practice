776beab28ec3ae9dd504f3e0d193ff6ed03a9667
optimisms <optimisms@users.noreply.github.com>
2022-11-30 09:07:06 -0700

    Added two paginated query methods

--- DynamoDBExerciseP2/src/main/java/org/example/Main.java
+++ DynamoDBExerciseP2/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -31,6 +32,20 @@ public class Main {
 
             //Delete 1 relationship from DB
             myDAO.deleteFollowerRelationship(relationship.follower_handle, relationship.followee_handle);
+
+            List<Relationship> first10followers = myDAO.getFollowers("@optimisms", 10, null);
+            System.out.println(first10followers);
+            System.out.println(first10followers.size());
+            List<Relationship> second10followers = myDAO.getFollowers("@optimisms", 10, first10followers.get(9).follower_handle);
+            System.out.println(second10followers);
+            System.out.println(second10followers.size());
+
+            List<Relationship> first10following = myDAO.getFollowing("@optimisms", 10, null);
+            System.out.println(first10following);
+            System.out.println(first10following.size());
+            List<Relationship> second10following = myDAO.getFollowing("@optimisms", 10, first10following.get(9).followee_handle);
+            System.out.println(second10following);
+            System.out.println(second10following.size());
 
             TimeUnit.SECONDS.sleep(10);
 
@@ -78,115 +93,5 @@ public class Main {
             relationship.setFollowee_name(fixedName);
             return relationship;//new Relationship(varHandle, varName, fixedHandle, fixedName);
         }
-    }
-
-    /**
-     * Fetch the next page of locations visited by visitor
-     *
-     * @param visitor The visitor of interest
-     * @param pageSize The maximum number of locations to include in the result
-     * @param lastLocation The last location returned in the previous page of results
-     * @return The next page of locations visited by visitor
-     */
-    public List<Visit> getVisitedLocations(String visitor, int pageSize, String lastLocation) {
-        DynamoDbTable<Visit> table = enhancedClient.table(TableName, TableSchema.fromBean(Visit.class));
-        Key key = Key.builder()
-                .partitionValue(visitor)
-                .build();
-
-        QueryEnhancedRequest.Builder requestBuilder = QueryEnhancedRequest.builder()
-                .queryConditional(QueryConditional.keyEqualTo(key));
-        // If you use iterators, it auto-fetches next page always, so instead limit the stream below
-        //.limit(5);
-
-        if(isNonEmptyString(lastLocation)) {
-            // Build up the Exclusive Start Key (telling DynamoDB where you left off reading items)
-            Map<String, AttributeValue> startKey = new HashMap<>();
-            startKey.put(VisitorAttr, AttributeValue.builder().s(visitor).build());
-            startKey.put(LocationAttr, AttributeValue.builder().s(lastLocation).build());
-
-            requestBuilder.exclusiveStartKey(startKey);
-        }
-
-        QueryEnhancedRequest request = requestBuilder.build();
-
-        return table.query(request)
-                .items()
-                .stream()
-                .limit(pageSize)
-                .collect(Collectors.toList());
-
-        /*
-         * Alternative Implementation 1 of the line above, using forEach
-         */
-        //List<Visit> visits = new ArrayList<>();
-        //table.query(request).items().stream().limit(pageSize).forEach(v -> visits.add(v));
-        //return visits;
-
-        /*
-         * Alternative Implementation 2 of the line above, using a for loop
-         */
-        //List<Visit> visits = new ArrayList<>();
-        //for(Visit visit : table.query(request).items()) {
-        //    // stop iteration if we've reached the number of items asked for
-        //    if(visits.size() >= pageSize)
-        //        break;
-        //    visits.add(visit);
-        //}
-        //return visits;
-
-        /*
-         * Alternative Implementation 3 of the line above, using a while loop
-         */
-        //List<Visit> visits = new ArrayList<>();
-        //Iterator<Visit> it = table.query(request).items().iterator();
-        //// while there are elements to iterate over, and we haven't reached the number of items asked for
-        //while(it.hasNext() && visits.size() < pageSize){
-        //    Visit visit = it.next();
-        //    visits.add(visit);
-        //}
-        //return visits;
-    }
-
-    /**
-     * Fetch the next page of visitors who have visited location
-     *
-     * @param location The location of interest
-     * @param pageSize The maximum number of visitors to include in the result
-     * @param lastVisitor The last visitor returned in the previous page of results
-     * @return The next page of visitors who have visited location
-     */
-    public List<Visit>  getVisitors(String location, int pageSize, String lastVisitor) {
-        DynamoDbIndex<Visit> index = enhancedClient.table(TableName, TableSchema.fromBean(Visit.class)).index(IndexName);
-        Key key = Key.builder()
-                .partitionValue(location)
-                .build();
-
-        QueryEnhancedRequest.Builder requestBuilder = QueryEnhancedRequest.builder()
-                .queryConditional(QueryConditional.keyEqualTo(key))
-                // Unlike Tables, querying from an Index returns a PageIterable, so we want to just ask for
-                // 1 page with pageSize items
-                .limit(pageSize);
-
-        if(isNonEmptyString(lastVisitor)) {
-            Map<String, AttributeValue> startKey = new HashMap<>();
-            startKey.put(LocationAttr, AttributeValue.builder().s(location).build());
-            startKey.put(VisitorAttr, AttributeValue.builder().s(lastVisitor).build());
-
-            requestBuilder.exclusiveStartKey(startKey);
-        }
-
-        QueryEnhancedRequest request = requestBuilder.build();
-
-        List<Visit> visits = new ArrayList<>();
-
-        SdkIterable<Page<Visit>> results2 = index.query(request);
-        PageIterable<Visit> pages = PageIterable.create(results2);
-        // limit 1 page, with pageSize items
-        pages.stream()
-                .limit(1)
-                .forEach(visitsPage -> visitsPage.items().forEach(v -> visits.add(v)));
-
-        return visits;
     }
 }
