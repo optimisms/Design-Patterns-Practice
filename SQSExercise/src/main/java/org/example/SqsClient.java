f222ede91477c706c3d8542f7d308426fd79cd4e
optimisms <optimisms@users.noreply.github.com>
2022-12-11 18:05:56 -0700

    Completed SQS exercise

--- SQSExercise/src/main/java/org/example/SqsClient.java
+++ SQSExercise/src/main/java/org/example/SqsClient.java
@@ -5,11 +5,9 @@ import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.amazonaws.services.sqs.model.SendMessageResult;
 
-
 public class SqsClient {
     public static void main(String[] args) {
-
-        String messageBody = "*** PUT YOUR MESSAGE BODY HERE ***";
+        String messageBody = "houston we have a problem";
         String queueUrl = "https://sqs.us-west-2.amazonaws.com/475409691518/sqs-exercise";
 
         SendMessageRequest send_msg_request = new SendMessageRequest()
---
+++ SQSExercise/src/main/java/org/example/sqs/QueueProcessor.java
@@ -0,0 +1,15 @@
+package org.example.sqs;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+
+public class QueueProcessor implements RequestHandler<SQSEvent, Void> {
+    @Override
+    public Void handleRequest(SQSEvent event, Context context) {
+        for (SQSEvent.SQSMessage msg : event.getRecords()) {
+            System.out.println(msg.getBody());
+        }
+        return null;
+    }
+}
