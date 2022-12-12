98bc0463cbc7e1af508cca25e41b05d8f5d67f10
optimisms <optimisms@users.noreply.github.com>
2022-12-11 21:59:54 -0700

    Initial commit

---
+++ CommandPatternExercise/src/main/java/org/example/texteditor/Main.java
@@ -0,0 +1,12 @@
+package org.example.texteditor;
+
+public class Main {
+    public static void main(String[] args) {
+        ISequence sequence = new Sequence();
+        Document document = new Document(sequence);
+
+        TextEditor editor = new TextEditor(document);
+
+        editor.run();
+    }
+}
