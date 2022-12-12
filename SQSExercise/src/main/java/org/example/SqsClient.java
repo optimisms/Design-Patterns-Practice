5a2abecbf6bbb8382625a1a124ca4a1cd2f7b817
optimisms <optimisms@users.noreply.github.com>
2022-12-11 17:23:31 -0700

    Initial commit

---
+++ SQSExercise/src/main/java/org/example/SqsClient.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
+
+
+public class SqsClient {
+    public static void main(String[] args) {
+
+        String messageBody = "*** PUT YOUR MESSAGE BODY HERE ***";
+        String queueUrl = "https://sqs.us-west-2.amazonaws.com/475409691518/sqs-exercise";
+
+        SendMessageRequest send_msg_request = new SendMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withMessageBody(messageBody);
+
+        AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+        SendMessageResult send_msg_result = sqs.sendMessage(send_msg_request);
+
+        String msgId = send_msg_result.getMessageId();
+        System.out.println("Message ID: " + msgId);
+    }
+}
